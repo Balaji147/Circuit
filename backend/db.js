517ec00c3e2718c7154065b40e1ada4cf4d622f9
Balaji Manickavasagam <mebala147@gmail.com>
2026-03-10 02:25:38 +0530

    Configured for production

--- backend/db.js
+++ backend/db.js
@@ -3,16 +3,27 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const pool = new Pool({
-    host:process.env.HOST,
-    user:process.env.USER,
-    port:process.env.DB_PORT,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DATABASE
+// const pool = new Pool({
+//     host:process.env.HOST,
+//     user:process.env.USER,
+//     port:process.env.DB_PORT,
+//     password:process.env.DB_PASSWORD,
+//     database:process.env.DATABASE
+// })
+
+// pool.connect().then(()=>{
+//     console.log("Connected at Circuit")
+// })
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
-pool.connect().then(()=>{
-    console.log("Connected at Circuit")
+pool.query("SELECT NOW()", (err, res) => {
+  console.log(err, res.rows)
 })
 
 export default pool
